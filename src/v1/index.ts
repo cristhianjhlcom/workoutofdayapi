13bf8808a13cfebaecd09b98fbb122c439b9f490
cristhianjhlcom <cristhianjhl.web@gmail.com>
2023-08-08 22:42:57 -0500

    Add v1 router file

---
+++ src/v1/index.ts
@@ -0,0 +1,9 @@
+import express, { Request, Response } from "express";
+
+const router = express.Router()
+
+router.route('/').get((req: Request, res: Response) => {
+    res.send(`<h2>Hello from ${req.baseUrl}</h2>`)
+});
+
+export default router;
