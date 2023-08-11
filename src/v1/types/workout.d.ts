6e5c13ef2c09aa2e734d3d3a9b5f43e629ebeb5f
cristhianjhlcom <cristhianjhl.web@gmail.com>
2023-08-10 22:25:57 -0500

    add workout type

---
+++ src/v1/types/workout.d.ts
@@ -0,0 +1,10 @@
+export type Workout = {
+	id: string;
+	name: string;
+	mode: string;
+	equipment: string[];
+	exercises: string[];
+	createdAt: string;
+	updatedAt: string;
+	trainerTips: string[];
+}
