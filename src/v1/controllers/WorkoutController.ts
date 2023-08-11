20969df1124b9f6b904b8f5c778657ddc700d457
cristhianjhlcom <cristhianjhl.web@gmail.com>
2023-08-10 22:25:22 -0500

    Remove constructor and import service instances

--- src/v1/controllers/WorkoutController.ts
+++ src/v1/controllers/WorkoutController.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from "express";
 import WorkoutService from "../services/WorkoutService";
+import { Workout } from "../types/workout";
 
 interface ControllerInterface {
-    service: WorkoutService;
     index: (req: Request, res: Response) => void;
     show: (req: Request, res: Response) => void;
     store: (req: Request, res: Response) => void;
@@ -11,43 +11,45 @@ interface ControllerInterface {
 }
 
 class WorkoutController implements ControllerInterface {
-    service: WorkoutService;
-
-    constructor() {
-        this.service = new WorkoutService();
-        console.log(new WorkoutService());
-    }
-
     // Listar todos los ejercicios.
     public index(_req: Request, res: Response) {
-        const workouts = this.service.list();
-        res.send('all workouts');
+        const workouts: Workout[] = WorkoutService.index();
+        res.send({
+            status: 'OK',
+            data: workouts,
+        });
     }
 
     // Mostrar un solo ejercicio por :id.
     public show(req: Request, res: Response) {
         const id: string = req.params.id;
-        const workout = this.service.getOneById();
-        res.send(`show workout with id ${id}`);
+        const workout = WorkoutService.show();
+        res.send({
+            status: 'OK',
+            data: workout
+        });
     }
 
     // Crear un nuevo ejercicio del dia.
-    public store(_req: Request, res: Response) {
-        this.service.create();
+    public store(req: Request, res: Response) {
+        const body: Omit<Workout, 'id' | 'createdAt' | 'updatedAt'> = req.body;
+        const { name, mode, equipment, exercises, trainerTips } = body;
+        console.log('peticion body', req.body);
+        WorkoutService.store();
         res.send('store workout');
     }
 
     // Actualizar un ejercicio por :id.
     public update(req: Request, res: Response) {
         const id: string = req.params.id;
-        this.service.updateOneById();
+        WorkoutService.update();
         res.send(`update workout with id ${id}`);
     }
 
     // Eliminar un ejercicio por :id.
     public destroy(req: Request, res: Response) {
         const id: string = req.params.id;
-        this.service.destroyOneById();
+        WorkoutService.destroy();
         res.send(`destroy workout with id ${id}`);
     }
 }
